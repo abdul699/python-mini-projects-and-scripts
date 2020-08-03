a7040687352445decf7ade25f80021bcca6b3657
Abdul <kabdulsalam6@gmail.com>
2020-08-03 16:03:31 +0530

    Added commnets for readibility

--- scripts/WhatsAppMessage.py
+++ scripts/WhatsAppMessage.py
@@ -1,3 +1,6 @@
 import pywhatkit
-
+# write the 10 digit mobile number inplace of xxx.. 
+# (+91) is country code (add your country code accordingly)
+# "This is a message" is the message you want to send
+# the last 2 parameters 15, 54 represents hours and minutes (eg, send message at 15hours and 54 minutes)
 pywhatkit.sendwhatmsg("+91xxxxxxxxxx", "This is a message", 15, 54)
